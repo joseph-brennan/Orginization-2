c0bb02f6aca0a50c44571330997487029d170e03
Joey <sharpshooter30@gmail.com>
2017-08-30 11:23:42 -0600

    working on structure

--- Homework 1.s
+++ Homework 1.s
@@ -1,12 +1,23 @@
         AREA    Homework 1, CODE, READONNLY
 
         ENTRY
-start
-        add value1, a, b
-        sub value2, a, b
+start:
+        add    value1, a, b
+        sub    value2, a, b
+        Mul    value3, a, b
+        add    value4, value1, value2, value3
 
-stop
+stop:
 
 
 
     END
+    
+    .data
+a: .word
+b: .word
+value1: .word
+value2: .word
+value3: .word
+value:  .word
+.align
