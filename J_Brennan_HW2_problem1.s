125a9b6571e3dc87a5932b51ad13eb687a83e7bd
Joseph-Brennan <sharpshooter30@gmail.com>
2017-09-24 15:43:54 -0600

    Done?

--- J_Brennan_HW2_problem1.s
+++ J_Brennan_HW2_problem1.s
@@ -1,11 +1,27 @@
   .equ SWI_Exit,  0x11   ; Set the value of SWI_Exit to 0x11
 
 start:
+    LDR R1, =string
+    MOV R0, #0
+
+loop:
+  LDRB R2, [R1], #1
+  CMP R2, #0
+  BEQ stop
+  ADD R0, R0, #1
+  B loop
+
+
 
 
 
 
 stop:
 
+    SWI   SWI_Exit         ; Stop program execution
+
     .data
+
+.align
 string:  .asciz  "This is my string"
+.end
--- J_Brennan_HW2_problem2.s
+++ J_Brennan_HW2_problem2.s
@@ -1,12 +1,24 @@
 .equ SWI_Exit,  0x11   ; Set the value of SWI_Exit to 0x11
 
 start:
+  ldr r3, =char
+  ldr r0, =string
+  ldrb r3, [r3]
+  mov r1, #0
 
+loop:
+  ldrb r2, [r0],#1
+  cmp r2, #0
+  beq done
+  cmp r2, r3
+  addeq r1,r1,#1
+  b loop
 
+done:
 
-
-stop:
+    SWI   SWI_Exit         ; Stop program execution
 
   .data
 string:  .asciz  "This is my string"
-char:    .asxiz  "T"
+char:    .asciz  "T"
+.end
