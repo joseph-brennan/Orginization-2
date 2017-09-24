6f7c8ad3de5b292368c2ff73a4932643c4d7ec5c
Joseph-Brennan <sharpshooter30@gmail.com>
2017-09-24 13:57:18 -0600

    starting hw2

---
+++ J_Brennan_HW2_problem1.s
@@ -0,0 +1,11 @@
+  .equ SWI_Exit,  0x11   ; Set the value of SWI_Exit to 0x11
+
+start:
+
+
+
+
+stop:
+
+    .data
+string:  .asciz  "This is my string"
---
+++ J_Brennan_HW2_problem2.s
@@ -0,0 +1,12 @@
+.equ SWI_Exit,  0x11   ; Set the value of SWI_Exit to 0x11
+
+start:
+
+
+
+
+stop:
+
+  .data
+string:  .asciz  "This is my string"
+char:    .asxiz  "T"
